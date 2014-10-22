4b3b1a5166c3c1f8c87e47e0c799c20a86ad2a76
Elin Nilsson <elin.nilsson30@gmail.com>
2014-10-22 14:08:03 +0200

    Building a html prototype for an android application with angularjs

---
+++ app/app.js
@@ -0,0 +1,26 @@
+﻿var app = angular.module('artnet', ['ngRoute']);
+
+    app.config(function ($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+        $routeProvider
+            .when('/about', 
+                {
+                    controller: 'sortsiteController',
+                    templateUrl: '/app/partials/about.html'
+                })
+            .when('/sorting', 
+                {
+                    controller: 'sortsiteController',
+                    templateUrl: '/app/partials/sorting.html'
+                })
+            .when('/', 
+                {
+                    controller: 'sortsiteController',
+                    templateUrl: '/app/partials/welcome.html'
+                })
+            .otherwise({ redirectTo: '/' });
+    });
+
+    app.config(function ($httpProvider) {
+        app.$httpProvider = $httpProvider;
+    });
